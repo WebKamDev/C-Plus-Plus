d6044adf4b938bda37116182eaf424fc7fd4e919
WebKamDev <raezil12@gmail.com>
2016-03-23 21:16:23 +0100

    Added files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a;
+    cout << "Hello world!" << endl;
+    cout << "Put your favourite number : ";
+    cin >> a;
+    cout << "\nYour favourite number is " << a << ".";
+    return 0;
+}
